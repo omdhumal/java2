ba082a3b6b87a62284cf74cdef2697eb2e110d2c
Om Dhumal <omdhumal1911@gmail.com>
2023-01-16 11:58:42 +0530

    add to existing file

--- ManageUserProfile2/src/main/java/com/service/UserServiceImpl.java
+++ ManageUserProfile2/src/main/java/com/service/UserServiceImpl.java
@@ -9,67 +9,66 @@ import com.dao.UserRepository;
 import com.model.User;
 
 @Service
-public class UserServiceImpl implements UserService{
-	
+public class UserServiceImpl implements UserService {
+
 	@Autowired
 	UserRepository userRepository;
 
 	@Override
 	public boolean verifyUserCredentials(String username, String password) {
-	
-		
+
 		User user = userRepository.getByCustomeremail(username);
-		
-		if(user != null && user.getCustomerpassword().equals(password)) {
+
+		if (user != null && user.getCustomerpassword().equals(password)) {
 			return true;
 		}
-		
+
 		return false;
-		
+
 	}
 
 	@Override
 	public User saveUserRecord(User user) {
-		
-		User user1=userRepository.save(user);
+
+		User user1 = userRepository.save(user);
 		return user1;
 	}
 
 	@Override
 	public List<User> getAllCustomers() {
-		List<User> list =userRepository.findAll();
+		List<User> list = userRepository.findAll();
 		return list;
 	}
 
 	@Override
 	public boolean DeleteDataById(int id) {
 		try {
-		userRepository.deleteById(id);
-		return true;
-		}catch(Exception e){
+			userRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
 			return false;
-			
+
 		}
 	}
 
 	@Override
 	public User getUser(int userId, User user) {
-		User user1=userRepository.getById(userId);
+		User user1 = userRepository.getById(userId);
 		user1.setCustomername(user.getCustomername());
 		user1.setCustomeremail(user.getCustomeremail());
 		user1.setCustomerpassword(user.getCustomerpassword());
 		user1.setCustomergender(user.getCustomergender());
 		user1.setCustomercountry(user.getCustomercountry());
 		user1.setContactno(user.getContactno());
-		
+
 		return userRepository.save(user1);
-		
+
 	}
 
 	@Override
 	public void updateUserbyId(User user) {
-		userRepository.UpdateByid (user.getCustomername(),user.getContactno(),user.getCustomerid());
-		
+		userRepository.UpdateByid(user.getCustomername(), user.getContactno(), user.getCustomerid());
+
 	}
 
 }
